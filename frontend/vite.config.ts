3cfbcf98eefcb31ac4bae9265eafdba797cf99a3
huong.ph-visualweber <huong.ph@fsofts.com>
2024-10-10 22:17:49 +0700

    save

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'react-router-dom': 'react-router-dom',
+    }
+  },
+  optimizeDeps: {
+    include: ['jquery', 'isotope-layout']
+  }
 })
