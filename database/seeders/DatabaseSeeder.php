60935d708ad346a728f3114545699412d959915e
huong.ph-visualweber <huong.ph@fsofts.com>
2024-10-10 22:00:29 +0700

    save

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,8 @@ class DatabaseSeeder extends Seeder
             ['name' => 'admin1', 'username' => 'admin1', 'email' => 'admin1@gmail.com'],
             ['name' => 'admin2', 'username' => 'admin2', 'email' => 'admin2@gmail.com'],
             ['name' => 'admin3', 'username' => 'admin3', 'email' => 'admin3@gmail.com'],
+            ['name' => 'admin4', 'username' => 'admin4', 'email' => 'admin4@gmail.com'],
+            ['name' => 'admin5', 'username' => 'admin5', 'email' => 'admin5@gmail.com'],
         ];
 
         foreach ($admins as $adminData) {
