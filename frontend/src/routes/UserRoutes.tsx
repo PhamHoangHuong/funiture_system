d5fe8592d25bd8126493a23cc5bccbcd30af1a4e
thanhphat0308 <nguyenphat38po@gmail.com>
2024-10-22 21:06:02 +0700

    product details

--- frontend/src/routes/UserRoutes.tsx
+++ frontend/src/routes/UserRoutes.tsx
@@ -2,18 +2,16 @@ import React from "react"
 import { Route, Routes } from "react-router-dom"
 import UserLayout from "../layouts/user/UserLayout"
 import Home from "../pages/user/Home/Home"
-// import ProductList from "../pages/user/product/ProductList"
 import UserProductLayout from "../pages/user/product/UserProductLayout"
+import ProductDetails from "../pages/user/product/ProductDetails"
 
 const UserRoutes: React.FC = () => {
     return (
         <Routes>
             <Route element={<UserLayout />}>
                 <Route index element={<Home />} />
-                <Route path="products" element={<UserProductLayout />}>
-                    {/* <Route index element={<ProductList />} /> */}
-                    {/* Thêm các route con khác cho sản phẩm ở đây nếu cần */}
-                </Route>
+                <Route path="products" element={<UserProductLayout />} />
+                <Route path="products/:id" element={<ProductDetails />} />
             </Route>
         </Routes>
     )
