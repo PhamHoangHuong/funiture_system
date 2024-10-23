e9f596184c0289073a22ed6f778eb874d11651a9
thanhphat0308 <nguyenphat38po@gmail.com>
2024-10-23 18:16:47 +0700

    map contact

--- frontend/src/pages/user/contact/Contact.tsx
+++ frontend/src/pages/user/contact/Contact.tsx
@@ -39,13 +39,12 @@ const Contact: React.FC = () => {
             <div className="col-xl-7 col-md-6">
               <div className="google-map-wrapper">
                 <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d155421.70603781365!2d13.259927382506678!3d52.506938616189004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a84e373f035901%3A0x42120465b5e3b70!2sBerlin%2C%20Germany!5e0!3m2!1sen!2sbd!4v1689504587526!5m2!1sen!2sbd"
-                  width="100%"
-                  height="600"
-                  style={{ border: 0 }}
-                  allowFullScreen
-                  loading="lazy"
-                  referrerPolicy="no-referrer-when-downgrade"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d501725.4184267668!2d106.36555353480477!3d10.755292873944828!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317529292e8d3dd1%3A0xf15f5aad773c112b!2zVGjDoG5oIHBo4buRIEjhu5MgQ2jDrSBNaW5oLCBI4buTIENow60gTWluaCwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1729682090729!5m2!1svi!2s" width="100%"
+                height="600"
+                style={{ border: 0 }}
+                allowFullScreen
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
                 ></iframe>
               </div>
             </div>
