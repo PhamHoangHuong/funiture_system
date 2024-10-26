d280465b3072c2ac7aba4ee44416ee766e242364
HoangHuong.dev <phamhoanghuong04@gmail.com>
2024-10-26 17:46:10 +0700

    add layout admin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,26 @@
 import React from "react"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
 import AdminRoutes from "./routes/AdminRoutes"
 import UserRouter from "./routes/UserRoutes"
 import { AuthProvider } from "./core/contexts/AuthContext"
-import './core/i18n/config';
+import './core/i18n/config'
+import theme from './theme'  // Import theme mới
 
 const App: React.FC = () => {
     return (
-        <Router>
-            <AuthProvider>
-                <Routes>
-                    <Route path="/admin/*" element={<AdminRoutes />} />
-                    <Route path="/*" element={<UserRouter />} />
-                </Routes>
-            </AuthProvider>
-        </Router>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Router>
+                <AuthProvider>
+                    <Routes>
+                        <Route path="/admin/*" element={<AdminRoutes />} />
+                        <Route path="/*" element={<UserRouter />} />
+                    </Routes>
+                </AuthProvider>
+            </Router>
+        </ThemeProvider>
     )
 }
 
