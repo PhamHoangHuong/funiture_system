bda4eccf819cb14429a53e89733b5e4f3e07dff8
thanhphat0308 <nguyenphat38po@gmail.com>
2024-11-03 12:45:42 +0700

    menu header

--- frontend/src/components/user/UserHeader.tsx
+++ frontend/src/components/user/UserHeader.tsx
@@ -24,6 +24,8 @@ export default function UserHeader() {
                                 <ul>
                                     <li><Link to="/">Home</Link></li>
                                     <li><Link to="/products">Products</Link></li>
+                                    <li><Link to="/blog">Blog</Link></li>
+                                    <li><Link to="/contact">Contact</Link></li>
                                 </ul>
                             </nav>
                         </div>
