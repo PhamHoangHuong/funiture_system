c4dcd995b0a49aba805fc7668fcbc40063ce5a02
Thạch Thế Đăng <124691462+dangthach1307@users.noreply.github.com>
2024-11-03 11:25:38 +0700

    Revert "Lux 71"

--- backend/Modules/CatalogPriceRule/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('catalogpricerule::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('catalogpricerule.name') !!}
-    </p>
-@endsection
