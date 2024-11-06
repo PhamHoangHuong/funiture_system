d94ef6914a076a5ca611dc9d1a73e054c4fc04d7
thanhphat0308 <nguyenphat38po@gmail.com>
2024-11-06 23:05:28 +0700

    update contact pro

--- frontend/src/pages/user/contact/Contact.tsx
+++ frontend/src/pages/user/contact/Contact.tsx
@@ -11,7 +11,6 @@ const Contact: React.FC = () => {
           </p>
         </div>
       </div>
-
       {/* Contact form section */}
       <section className="contact-form-section pt-20 pb-20 overflow-hidden">
         <div className="container-1700">
@@ -19,7 +18,7 @@ const Contact: React.FC = () => {
             <div className="col-xl-7 col-md-6">
               <div className="google-map-wrapper">
                 <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d501725.4184267668!2d106.36555353480477!3d10.755292873944828!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317529292e8d3dd1%3A0xf15f5aad773c112b!2zVGjDoG5oIHBo4buRIEjhu5MgQ2jDrSBNaW5oLCBI4buTIENow60gTWluaCwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1729682090729!5m2!1svi!2s" width="100%"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.5414698974473!2d106.63467557430442!3d10.846359457901357!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752922c4467389%3A0x2a2d40bf0b428a91!2zMTM5MiDEkC4gUXVhbmcgVHJ1bmcsIFBoxrDhu51uZyAxNCwgR8OyIFbhuqVwLCBI4buTIENow60gTWluaCwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1730904670843!5m2!1svi!2s" width="100%"
                 height="600"
                 style={{ border: 0 }}
                 allowFullScreen
