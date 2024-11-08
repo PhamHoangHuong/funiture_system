8b66ddeb18c7143c78f43e75e15cb9b693787b5a
The Dang <dangthach1307@gmail.com>
2024-11-08 09:53:06 +0700

    LUX-75 Add Module Wishlist

---
+++ backend/Modules/Wishlist/Database/Migrations/2024_11_08_015248_create_wishlists_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wishlists', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('customer_id')->nullable()->comment('Customer ID');
+            $table->unsignedBigInteger('product_id')->nullable()->comment('Product ID');
+            $table->timestamps();
+
+            $table->foreign('customer_id')->references('id')->on('customers')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wishlists');
+    }
+};
